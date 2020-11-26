26d7928e7d08ebc6a28666cbcaa14410585f4914
wongprom <>
2020-11-26 22:07:25 +0100

    Created Signin UI.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './header/Header';
 import Home from './home/Home';
 import Checkout from './chekout/Checkout';
+import Login from './login/Login';
 
 import './App.css';
 
@@ -10,12 +11,16 @@ function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
         <Switch>
+          <Route path="/login">
+            <Login>Login</Login>
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
